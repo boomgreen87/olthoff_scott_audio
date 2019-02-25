0a7b1a47ba55ce62ccaa660faf5ddef552f2cdc8
Olthoff <s_olthoff2@tss-hr420-dm26.fc.ca>
2019-02-25 15:45:06 -0500

    Some more stuff

--- js/main.js
+++ js/main.js
@@ -3,23 +3,33 @@
 	let aud = document.querySelector('audio'),
 		play = document.querySelector('#playButton'),
 		pause = document.querySelector('#pauseButton'),
-		rewind = document.querySelector('#rewindButton');
+		rewind = document.querySelector('#rewindButton'),
+		tracks = document.querySelectorAll('.trackHolder');
 
 		//Write the functions for the audio element
-		function playAudio(){
+		function playAudio() {
 			aud.play();
 		}
 
-		function pauseAudio(){
+		function pauseAudio() {
 			aud.pause(true);
 		}
 
-		function rewindAudio(){
+		function rewindAudio() {
 			aud.currentTime = 0;
+		}
+
+		function swapSource() {
+			let currentTrack = this.dataset.currenttrack;
+
+			aud.src = `audio/${currentTrack}`;
+			aud.load();
+			aud.play();
 		}
 
 		//Add event handlers
 		play.addEventListener("click", playAudio);
 		pause.addEventListener("click", pauseAudio);
 		rewind.addEventListener("click", rewindAudio);
+		tracks.forEach(track => track.addEventListener("click", swapSource));
 })();
